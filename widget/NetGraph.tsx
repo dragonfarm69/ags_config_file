93b98311ec4c1d1bc0ba42503f95485f6824d60f
dragonfarm69 <vuminhlongcm123@gmail.com>
2025-12-11 02:10:47 +0700

    fix bug where net graph doesn't update after a single spike

--- widget/NetGraph.tsx
+++ widget/NetGraph.tsx
@@ -9,26 +9,28 @@ type NetGraphProps = {
 };
 
 export const NetGraph = ({ width = 250, height = 70, maxSamples = 60 }: NetGraphProps) => {
-  let downHistory = Array(maxSamples).fill(0);
-  let upHistory = Array(maxSamples).fill(0);
+    let downHistory = Array(maxSamples).fill(0);
+    let upHistory = Array(maxSamples).fill(0);
+    downloadSpeed.subscribe(() => area.queue_draw());
+    uploadSpeed.subscribe(() => area.queue_draw());
+    
+    const area = new Gtk.DrawingArea({
+        width_request: width,
+        height_request: height,
+    });
 
-  const area = new Gtk.DrawingArea({
-    width_request: width,
-    height_request: height,
-  });
+    const updateHistory = () => {
+        downHistory.push(downloadSpeed.get());
+        upHistory.push(uploadSpeed.get());
+        if (downHistory.length > maxSamples) downHistory.shift();
+        if (upHistory.length > maxSamples) upHistory.shift();
+        area.queue_draw();
+        return true;
+    };
 
-  const updateHistory = () => {
-    downHistory.push(downloadSpeed.get());
-    upHistory.push(uploadSpeed.get());
-    if (downHistory.length > maxSamples) downHistory.shift();
-    if (upHistory.length > maxSamples) upHistory.shift();
-    area.queue_draw();
-    return true;
-  };
+    GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, updateHistory);
 
-  GLib.timeout_add(GLib.PRIORITY_DEFAULT, 1000, updateHistory);
-
-  area.set_draw_func((_, cr, w, h) => {
+    area.set_draw_func((_, cr, w, h) => {
     cr.setSourceRGBA(0.1, 0.1, 0.1, 0.5);
     cr.rectangle(0, 0, w, h);
     cr.fill();
@@ -40,9 +42,9 @@ export const NetGraph = ({ width = 250, height = 70, maxSamples = 60 }: NetGraph
     cr.setSourceRGBA(1, 0, 0, 1);
     cr.setLineWidth(2);
     downHistory.forEach((v, i) => {
-      const x = (i / (maxSamples - 1)) * w;
-      if (i === 0) cr.moveTo(x, scale(v));
-      else cr.lineTo(x, scale(v));
+        const x = (i / (maxSamples - 1)) * w;
+        if (i === 0) cr.moveTo(x, scale(v));
+        else cr.lineTo(x, scale(v));
     });
     cr.stroke();
 
@@ -50,12 +52,12 @@ export const NetGraph = ({ width = 250, height = 70, maxSamples = 60 }: NetGraph
     cr.setSourceRGBA(0, 1, 0, 1);
     cr.setLineWidth(2);
     upHistory.forEach((v, i) => {
-      const x = (i / (maxSamples - 1)) * w;
-      if (i === 0) cr.moveTo(x, scale(v));
-      else cr.lineTo(x, scale(v));
+        const x = (i / (maxSamples - 1)) * w;
+        if (i === 0) cr.moveTo(x, scale(v));
+        else cr.lineTo(x, scale(v));
     });
     cr.stroke();
-  });
+    });
 
-  return area;
+    return area;
 };
